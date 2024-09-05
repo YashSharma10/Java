27e1cff71b37d39fbfc74d263ef8fdbc59675dda
YashSharma10 <yashsharma67953@gmail.com>
2024-09-05 11:28:09 +0530

    ..

--- OS/FCFSWithArrival.java
+++ OS/FCFSWithArrival.java
@@ -1,27 +1,31 @@
 public class FCFSWithArrival {
     public static void main(String[] args) {
-        int[] arrivalTime = { 0, 2, 3, 5, 6 };
-        int[] burstTime = { 4, 3, 1, 2, 3 };
+        int[] arrivalTime = { 0, 2, 7, 5, 6 };
+        int[] burstTime = { 4, 4, 1, 5, 3 };
         int n = arrivalTime.length;
-
         int[] waitingTime = new int[n];
         int[] turnAroundTime = new int[n];
 
         waitingTime[0] = 0;
         turnAroundTime[0] = burstTime[0];
 
+        float Wait = 0;
+        float Turn = 0;
+
         for (int i = 1; i < n; i++) {
-            waitingTime[i] = turnAroundTime[i - 1] - arrivalTime[i];
-            if (waitingTime[i] < 0) {
-                waitingTime[i] = 0;
-                turnAroundTime[i] = waitingTime[i] + burstTime[i];
-            }
+            waitingTime[i] = Math.max(0, Math.max(arrivalTime[i], turnAroundTime[i - 1]) - arrivalTime[i]);
+            turnAroundTime[i] = waitingTime[i] + burstTime[i];
+            Wait += waitingTime[i];
+            Turn += turnAroundTime[i];
         }
+        float avgWait =  Wait / n;
+        float avgTurn = Turn / n;
 
         System.out.println("Process\tArrival\tBurst\tWaiting\tTurnaround");
         for (int i = 0; i < n; i++) {
-            System.out.println((i + 1) + "\t" + arrivalTime[i] + "\t" + burstTime[i] + "\t" + waitingTime[i] + "\t"
-                    + turnAroundTime[i]);
+            System.out.println((i + 1) + "\t" + arrivalTime[i] + "\t" + burstTime[i] + "\t" + waitingTime[i] + "\t"+ turnAroundTime[i]);
         }
+        System.out.println("Average Wait time :" + avgWait);
+        System.out.println("Average TurnAround Time :" + avgTurn);
     }
 }
--- OS/FCFSWithoutArrival.java
+++ OS/FCFSWithoutArrival.java
@@ -1,6 +1,6 @@
 public class FCFSWithoutArrival {
     public static void main(String[] args) {
-        int[] burstTime = { 4, 3, 1, 2, 3 };
+        int[] burstTime = { 4, 4, 1, 5, 3 };
         int n = burstTime.length;
 
         int[] waitingTime = new int[n];
@@ -9,14 +9,24 @@ public class FCFSWithoutArrival {
         waitingTime[0] = 0;
         turnAroundTime[0] = burstTime[0];
 
+        float Wait = 0;
+        float Turn = 0;
+
         for (int i = 1; i < n; i++) {
             waitingTime[i] = turnAroundTime[i - 1];
             turnAroundTime[i] = waitingTime[i] + burstTime[i];
+            Wait += waitingTime[i];
+            Turn += turnAroundTime[i];
         }
+        float avgWait = Wait/n;
+        float avgTurn = Turn/n;
 
         System.out.println("Process\tBurst\tWaiting\tTurnaround");
         for (int i = 0; i < n; i++) {
             System.out.println((i + 1) + "\t" + burstTime[i] + "\t" + waitingTime[i] + "\t" + turnAroundTime[i]);
         }
+        System.out.println("Average Wait time :"+avgWait);
+        System.out.println("Average TurnAround Time :"+avgTurn);
+
     }
 }
